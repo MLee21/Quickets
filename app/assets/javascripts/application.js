c19ab3171aad6400b73485b21747beb6b7ba21d5
MLee21 <minnie.lee89@gmail.com>
2015-06-13 11:59:16 -0600

    configured angular, foundation, bower, and rspec. removed turbolinks. added necessary gems for testing and installed and setup.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,64 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require_tree .
+//= require angular
+//= require angular-rails-templates
+
+//= require angular/app
+//= require_tree ./angular-app/templates
+//= require_tree ./angular-app/modules
+//= require_tree ./angular-app/filters
+//= require_tree ./angular-app/directives
+//= require_tree ./angular-app/models
+//= require_tree ./angular-app/services
+//= require_tree ./angular-app/controllers
+
+$(function(){ $(document).foundation(); });
+
+'use strict';
+ 
+/**
+ * @ngdoc overview
+ * @name ngFoundationApp
+ * @description
+ * # ngFoundationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngFoundationApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'mm.foundation'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
