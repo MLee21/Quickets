4a68077e8cd83e0fca03db9789be735666775c55
MLee21 <minnie.lee89@gmail.com>
2015-06-21 15:33:30 -0600

    added angular to sort and filter data. converted artist objects to json for angular. deleted bower.json - don't need. added moment.js to parse date.

--- app/services/bands_in_town_service.rb
+++ app/services/bands_in_town_service.rb
@@ -5,11 +5,6 @@ class BandsInTownService
     @connection = Hurley::Client.new("http://api.bandsintown.com")
   end
 
-  def get_artist_events(name)
-    response = connection.get("/artists/#{name}/events.json?api_version=2.0&app_id=Quickets&")
-    response_body = parse(response.body)
-  end
-
   def nearby_artists_at_events(location)
     response = connection.get("/events/search.json", {api_version: 2.0, app_id: "Quickets", location: location})
     response_body = parse(response.body)
