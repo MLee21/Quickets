ee8d43985c2d23e50101ee442233a41d1fc1b1d6
MLee21 <minnie.lee89@gmail.com>
2016-06-21 16:41:12 -0600

    set full host in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
   scope: ['email',
-    'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'}
+  'https://www.googleapis.com/auth/gmail.modify'],
+  access_type: 'offline'
+  }
 end
+
+OmniAuth.config.full_host = Rails.env.production? ? 'http://quickets.herokuapp.com' : 'http://localhost:3000'
